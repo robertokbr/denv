4c9d6b08a5f3d908349ec7545ce23433e329ba87
robertokbr <robertojuniordev@gmail.com>
2024-02-22 14:20:06 -0300

    feat: add s3 bucket

---
+++ bucket/s3.go
@@ -0,0 +1,102 @@
+package bucket
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+type S3Bucket struct {
+	bucket     *s3.S3
+	bucketName string
+}
+
+func NewS3Bucket() *S3Bucket {
+	accessKey := os.Getenv("AWS_ACCESS_KEY")
+	secretKey := os.Getenv("AWS_SECRET_KEY")
+	bucketName := os.Getenv("AWS_BUCKET_NAME")
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
+	}))
+
+	bucket := s3.New(sess)
+
+	s3Bucket := S3Bucket{
+		bucket:     bucket,
+		bucketName: bucketName,
+	}
+
+	return &s3Bucket
+}
+
+func (s3b *S3Bucket) UploadFile(filePath, name string) {
+	fmt.Println("🚚 Upload in process...")
+
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	defer file.Close()
+
+	fileStat, _ := file.Stat()
+	size := fileStat.Size()
+	buffer := make([]byte, size)
+	file.Read(buffer)
+
+	_, err = s3b.bucket.PutObject(&s3.PutObjectInput{
+		Bucket:             aws.String(s3b.bucketName),
+		Key:                aws.String(fmt.Sprintf("%s.txt", name)),
+		Body:               strings.NewReader(string(buffer)),
+		ACL:                aws.String("private"),
+		ContentDisposition: aws.String("attachment"),
+		ContentType:        aws.String("application/octet-stream"),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to upload file to s3: %v", err)
+	}
+
+	fmt.Println("🥳 Filed uploaded!!!")
+}
+
+func (s3b *S3Bucket) DownloadFile(name string) {
+	fmt.Println("🚚 Download in process...")
+
+	res, err := s3b.bucket.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(s3b.bucketName),
+		Key:    aws.String(fmt.Sprintf("%s.txt", name)),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to download the file: %v", err)
+	}
+
+	defer res.Body.Close()
+
+	file, err := os.Create(fmt.Sprintf("%s.txt", name))
+
+	if err != nil {
+		log.Fatalf("Failed to create env file: %v", err)
+	}
+
+	defer file.Close()
+
+	_, err = io.Copy(file, res.Body)
+
+	if err != nil {
+		log.Fatalf("Failed to download file: %v", err)
+	}
+
+	fmt.Println("🥳 Download succeed!!!")
+}
